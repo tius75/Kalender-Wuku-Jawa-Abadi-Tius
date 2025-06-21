9862e29b9ab601944a96142ee3329545e775436d
tius75 <76941855+tius75@users.noreply.github.com>
2025-06-21 09:06:15 +0700

    Upload pertama

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'kalender-jawa-v1';
+const urlsToCache = [
+  './', // Ini penting untuk meng-cache root directory (index.html)
+  './index.html',
+  './manifest.json',
+  './favicon32.png',
+  './logo192.png',
+  './logo.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
